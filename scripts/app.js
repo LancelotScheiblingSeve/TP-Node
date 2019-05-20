03ad4cb46103d337ab4cc31ba6f3a7abc1f452d1
DarkRevan1 <lancelot.scheibling.seve@gmail.com>
2019-05-20 12:36:14 +0200

    scripts

--- scripts/app.js
+++ scripts/app.js
@@ -7,7 +7,16 @@ formSearch.addEventListener('submit', event => {
     // console.log('Formulaire validé')
     const city = inputCityName.value;
     WeatherServise.getWeatherCity(city).then(weather => {
+        //results.innerHTML = `<div class="card" style="width: 22rem;">
+        //                        <div class="card-body">
+        //                           <h5 class="card-title">${weather.name}, ${weather.sys.country}</h5>
+        //                        </div>
+        //                    </div>`;
         results.innerHTML = `<div class="card" style="width: 22rem;">
+                                ${weather.weather.map(w => (
+                                    `<img src="http://openweathermap.org/img/w/${w.icon}.png" class="float-right">`
+                                )).join('')}
+                                
                                 <div class="card-body">
                                     <h5 class="card-title">${weather.name}, ${weather.sys.country}</h5>
                                 </div>
