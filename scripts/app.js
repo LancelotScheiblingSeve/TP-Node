116a65ef03e4770f2825656e12953360c671dfed
DarkRevan1 <lancelot.scheibling.seve@gmail.com>
2019-05-20 12:58:52 +0200

    scripts

--- scripts/app.js
+++ scripts/app.js
@@ -7,19 +7,38 @@ formSearch.addEventListener('submit', event => {
     // console.log('Formulaire validé')
     const city = inputCityName.value;
     WeatherServise.getWeatherCity(city).then(weather => {
+        console.log(weather);
         //results.innerHTML = `<div class="card" style="width: 22rem;">
         //                        <div class="card-body">
         //                           <h5 class="card-title">${weather.name}, ${weather.sys.country}</h5>
         //                        </div>
         //                    </div>`;
+        // results.innerHTML = `<div class="card" style="width: 22rem;">
+        //         <div class="card-body">
+        //             ${weather.weather.map(w => (
+        //                 `<img src="http://openweathermap.org/img/w/${w.icon}.png" class="float-right">`
+        //             )).join('')}
+        //             <h5 class="card-title">${weather.name}, ${weather.sys.country}</h5>
+        //             <p class="card-title">${weather.main.temp}°C,</p>
+        //             <p class="card-title">Max : ${weather.main.temp_max}°C,</p>
+        //             <p class="card-title">Min : ${weather.main.temp_min}°C</p>
+        //         </div>
+        // </div>`;
         results.innerHTML = `<div class="card" style="width: 22rem;">
+                        <div class="card-body">
+                            ${weather.weather.map(w => (
+                                `<img src="http://openweathermap.org/img/w/${w.icon}.png" alt="${w.description}" class="float-right">`
+                            )).join('')}
+                            <h5 class="card-title">${weather.name}, ${weather.sys.country}</h5>
+                            <h6 class="card-subtitle mb-2 text-muted">
                                 ${weather.weather.map(w => (
-                                    `<img src="http://openweathermap.org/img/w/${w.icon}.png" class="float-right">`
-                                )).join('')}
-                                
-                                <div class="card-body">
-                                    <h5 class="card-title">${weather.name}, ${weather.sys.country}</h5>
-                                </div>
-                            </div>`;
+                                    `<strong>${w.description}</strong>`
+                                )).join(', ')}
+                                <br>
+                            </h6>
+                            <p class="card-text">Temperature : ${weather.main.temp}°C</p>
+                            <a href="#" class="card-link" id="previsions" data-city-id="${weather.id}">Afficher les prévisions</a>
+                        </div>
+                    </div>`;
     });
 });
